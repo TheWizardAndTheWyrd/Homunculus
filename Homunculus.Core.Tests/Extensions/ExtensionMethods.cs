b9cb8989beccd6027565c251cc302ac7324b4aca
Ramon Long <ramonjlongiii@gmail.com>
2015-10-18 00:29:43 -0400

    Adding new test for multiple value dot product multiplication via LINQ.

--- Homunculus.Core.Tests/Extensions/ExtensionMethods.cs
+++ Homunculus.Core.Tests/Extensions/ExtensionMethods.cs
@@ -28,5 +28,29 @@ namespace Homunculus.Core.Tests.Extensions
             Assert.AreEqual(0.05487416F, result);
             Console.WriteLine("Result of DotProduct invocation: {0}", result);
         }
+
+        [TestMethod]
+        public void MultipleValueDotProductTest()
+        {
+            // Arrange
+            float v1 = 0.2342523523523452345F;
+            float v2 = 0.2342523523523452345F;
+
+            float v3 = 0.8298239857283975982F;
+            float v4 = 0.8298239857283975982F;
+
+            List<float> leftValues = new List<float>() {v1, v3};
+            List<float> rightValues = new List<float>() {v2, v4};
+
+            float result;
+
+            // Act
+            result = leftValues.DotProduct(rightValues);
+
+            // Assert
+            Assert.AreNotEqual(0, result);
+            Assert.AreEqual(0.743482F, result);
+            Console.WriteLine("Result of DotProduct invocation: {0}", result);
+        }
     }
 }
