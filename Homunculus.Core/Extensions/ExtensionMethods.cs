4023ba53259ab84a57e6b7c8a26c911e636c2bb5
Ramon Long <ramonjlongiii@gmail.com>
2015-10-18 00:25:34 -0400

    Initial commit.

---
+++ Homunculus.Core/Extensions/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Homunculus.Core.Extensions
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Implement a dot product method using LINQ.
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static float DotProduct(this IEnumerable<float> v1, IEnumerable<float> v2)
+        {
+            return v1.Zip(v2, (a, b) => a*b).Sum();
+        }
+    }
+}
---
+++ Homunculus.Core/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homunculus.Core
+{
+    public class Neuron
+    {
+        #region [ Private Fields ]
+        private Random _random;
+        private Tuple<float, float, float> _weights;
+        #endregion
+
+        #region [ Public Properties ]
+
+        #endregion
+
+        #region [ Constructors ]
+        public Neuron(int seed, Tuple<float, float, float> weights)
+        {
+            _random = new Random(seed);
+            _weights = weights;
+            #endregion
+        }
+    }
+}
