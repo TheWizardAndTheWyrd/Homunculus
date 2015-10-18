1502bca62fc23c31a9eb539a7dce522e16d82983
Ramon Long <ramonjlongiii@gmail.com>
2015-10-18 05:16:13 -0400

    Working on NeuronActor and learning about changes to the Akka.NET API since 1.x was released (I haven't used Akka.NET since pre-release in beta).

---
+++ Homunculus.Core.Tests/Actors/NeuronActorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Routing;
+using Homunculus.Core.Actors;
+using Homunculus.Core.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homunculus.Core.Tests.Actors
+{
+    [TestClass]
+    public class NeuronActorTests
+    {
+        private static ActorSystem _actorSystem;
+        private static IActorRef _testActor;
+        private static Props _props;
+
+        [TestMethod]
+        public void SendNeuronToActorSystem()
+        {
+            // Arrange
+            INeuron neuronMessage = new Neuron(seed: 1, accumulator: 0);
+
+            _actorSystem = ActorSystem.Create("neuronTestSystem");
+            _props = Props.Create<NeuronActor>().WithRouter(new RoundRobinPool(5));
+            _testActor = _actorSystem.ActorOf(_props, "neuron");
+
+            // Act
+            //Task<object> t = Task.Run(async () =>
+            //{
+            //    var t1 = _testActor.Ask(neuronMessage, TimeSpan.FromSeconds(1));
+
+            //    await Task.WhenAll(t1);
+
+            //    return t1.Result;
+            //});
+
+            _testActor.Tell(neuronMessage, _testActor);
+
+            // Assert
+            //Console.WriteLine("The result of sending our neuronActor to the neuronTestSystem: {0}", t.Result);
+            Assert.Fail("The Actor System is not receiving neurons.  The actor system should probably spawn INeuron objects.");
+        }
+    }
+}
--- Homunculus.Core/Actors/NeuronActor.cs
+++ Homunculus.Core/Actors/NeuronActor.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using Akka.Actor;
+using Akka.Event;
 using Homunculus.Core.Interfaces;
 
 namespace Homunculus.Core.Actors
 {
     public class NeuronActor : ReceiveActor
     {
+        private readonly ILoggingAdapter _log = Context.GetLogger();
+
         public NeuronActor()
         {
             Receive<INeuron>(neuron =>
             {
+                // Successive invocations of neuron.Output might mutate state.
                 var neuronOutput = neuron.Output;
+
                 Console.WriteLine("Neuron Output: {0}", ((Neuron)neuron).Output);
+
+                Sender.Tell(neuronOutput, Self);
+
+                _log.Info("The result of our neuron's computation: {0}", neuronOutput);
             });
         }
     }
