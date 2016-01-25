b022ef2c986731e539c79691c971970f7b3eb237
Ramon Long <ramonjlongiii@gmail.com>
2016-01-25 04:56:35 -0500

    Adding our BlockExpressionActor.

---
+++ Homunculus.Core.Actors/BlockExpressionActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Homunculus.Core.Actors
+{
+    public class BlockExpressionActor<T, TResult> : ReceiveActor
+    {
+        public BlockExpressionActor()
+        {
+            Receive<Tuple<BlockExpression, ParameterExpression>>(message =>
+            {
+                try
+                {
+                    var d = Expression.Lambda<Func<T, TResult>>(message.Item1, message.Item2).Compile();
+                    Sender.Tell(Task.FromResult(d), Self);
+                }
+                catch (Exception e)
+                {
+                    Sender.Tell(new Failure { Exception = e }, Self);
+                }
+            });
+        }
+    }
+}
